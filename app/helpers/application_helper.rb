4357f25331fea4331cd60eb4e103ca77695c652c
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-02 23:57:12 -0800

    Added title helper to application_helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def full_title(title)
+    full_title = ''
+    full_title = if title
+                   "#{title} | Invoice App"
+                 else
+                   'Invoice App'
+                 end
+    full_title
+  end
 end
