47af47b73a6abf4290ba0aa64d2694bcfb178821
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-04 16:56:08 -0800

    Added Associations between user and invoice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_04_071944) do
+ActiveRecord::Schema.define(version: 2022_02_05_005348) do
 
   create_table "invoice_items", force: :cascade do |t|
     t.date "date"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2022_02_04_071944) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_invoices_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +60,5 @@ ActiveRecord::Schema.define(version: 2022_02_04_071944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "invoices", "users"
 end
