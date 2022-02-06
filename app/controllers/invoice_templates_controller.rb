96d8579a0e897b16366d3af811c34e55025c4349
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-06 01:02:26 -0800

    Finished adding invoice template creation

--- app/controllers/invoice_templates_controller.rb
+++ app/controllers/invoice_templates_controller.rb
@@ -2,4 +2,28 @@ class InvoiceTemplatesController < ApplicationController
   def new
     @invoice_template = InvoiceTemplate.new
   end
+
+  def create
+    @invoice_template = current_user.invoice_templates.build(invoice_template_params)
+    if @invoice_template.save
+      flash[:success] = 'Invoice Template Created!'
+      redirect_to new_invoice_path
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  private
+
+  def invoice_template_params
+    params.require(:invoice_template).permit(:to_full_name,
+                                             :to_email,
+                                             :to_phone,
+                                             :to_address,
+                                             :to_city,
+                                             :to_country,
+                                             :to_province,
+                                             :to_postal_code,
+                                             :notes)
+  end
 end
