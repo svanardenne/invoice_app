05131e4937c5450fd399215c17137b94a01cb084
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-11 10:42:11 -0800

    Changed settings to profile in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :invoice_templates, dependent: :destroy
-  has_one :settings, dependent: :destroy
-  has_one :profile
+  has_one :profile, dependent: :destroy
   before_create :build_associations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -12,13 +11,13 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true
 
-  def setting
-    super || build_setting(current_template: :null)
+  def profile
+    super || build_profile(current_template: :null)
   end
 
   private
 
   def build_associations
-    setting || true
+    profile || true
   end
 end
