eab36a06bd2bc690c5e80d9fc7ea5953a3ad4733
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-11 10:16:50 -0800

    Added code to user.rb model to automatically set up settings on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :invoices, dependent: :destroy
   has_many :invoice_templates, dependent: :destroy
   has_one :settings, dependent: :destroy
+  has_one :profile
+  before_create :build_associations
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,4 +11,14 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   validates :first_name, presence: true
+
+  def setting
+    super || build_setting(current_template: :null)
+  end
+
+  private
+
+  def build_associations
+    setting || true
+  end
 end
