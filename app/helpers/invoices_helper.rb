17c66e5f2fa78a7bd8d00ad63d211961133ea945
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-11 15:34:44 -0800

    Added to_field helper

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,7 @@
 module InvoicesHelper
+  def to_field(field)
+    unless current_user.profile.current_template.nil?
+      current_user.invoice_templates[current_user.profile.current_template - 1][field]
+    end
+  end
 end
