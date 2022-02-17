80dcaf55f9e101b22b7a81d1c10918df92f01983
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-17 12:39:39 -0800

    Added model validations for invoice templates

--- app/models/invoice_template.rb
+++ app/models/invoice_template.rb
@@ -1,3 +1,6 @@
 class InvoiceTemplate < ApplicationRecord
   belongs_to :user
+
+  validates :template_name, presence: { message: 'Template Name is required.' }
+  validates :to_full_name, presence: { message: 'To Full Name is required.' }
 end
