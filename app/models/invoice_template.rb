43064daeb7a30e825e1ffc786c778a9161634cdc
Shaun van Ardenne <s.vanardenne@gmail.com>
2022-02-17 13:44:08 -0800

    Added uniqueness attribute to template name

--- app/models/invoice_template.rb
+++ app/models/invoice_template.rb
@@ -1,6 +1,6 @@
 class InvoiceTemplate < ApplicationRecord
   belongs_to :user
 
-  validates :template_name, presence: true
+  validates :template_name, presence: true, uniqueness: true
   validates :to_full_name, presence: true
 end
